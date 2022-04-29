bf428614ee1de98d7ff1a5d80161306eee51b5b2
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-29 03:07:41 +0100

    Update financial.py

--- financial.py
+++ financial.py
@@ -26,7 +26,7 @@ def init_connection():
     return pyodbc.connect(
         'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
 
-    conn = init_connection()
+  conn = init_connection()
 
 
 @st.experimental_memo(ttl=600)
