2c5d9ba08bf3f88a3d57b1d911203923d664b7a3
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-29 22:15:32 +0100

    Update financial.py

--- financial.py
+++ financial.py
@@ -12,18 +12,18 @@ pink_data = st.sidebar.selectbox("choose", ('Choose', 'Database', 'Database Conn
 
 
 if pink_data == 'Database':
-    form = st.form("forms", clear_on_submit=True)
-    id = form.text_input("Company's ID")
-    date = form.date_input("Today's Date",
+    with st.form("forms", clear_on_submit=True):
+         id_name = st.text_input("Company's ID")
+         date = st.date_input("Today's Date",
                       datetime.date(2022, 4, 1))
-    name = form.text_input("Company's Name")
-    amount = form.text_input("Amount")
-    payment = form.text_input("Payment Mode")
-    submit = form.form_submit_button("Submit")
+         name = st.text_input("Company's Name")
+         amount = st.text_input("Amount")
+         payment = st.text_input("Payment Mode")
+         submit = st.form_submit_button("Submit")
 deta = Deta(st.secrets["deta_key"])
 db = deta.Base("Financial-records")
 if submit:
-     db.put({"company_id": id, "date": date, "company_name":name, "amount":amount, "payment_mode":payment})
+     db.put({"company_id": id_name, "date": date, "company_name":name, "amount":amount, "payment_mode":payment})
   
 
 
