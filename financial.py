87a6125504783bf881230edf4555427ea22667b7
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-29 22:30:37 +0100

    Update financial.py

--- financial.py
+++ financial.py
@@ -25,9 +25,9 @@ if pink_data == 'Database':
 deta = Deta(st.secrets["deta_key"])
 db = deta.Base("Financial-records")
 json.loads(data, default=json_util.default)
-data = {"company_id": id_name, "date": date, "company_name":name, "amount":amount, "payment_mode":payment}
 if submit:
-     db.put(data)
+     db.put({"company_id": id_name, "date": date, "company_name":name, "amount":amount, "payment_mode":payment})
+
  
 "---"
 
