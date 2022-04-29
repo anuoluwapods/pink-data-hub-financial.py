69347d8f51527fd49b62f1bef1b5e02537b9a42c
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-29 02:39:42 +0100

    Update financial.py

--- financial.py
+++ financial.py
@@ -10,16 +10,7 @@ st.header("Pink Data Hub Financial App")
 @st.experimental_singleton
 def init_connection():
     return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        +st.secrets["server"]
-        +";DATABASE="
-        +st.secrets["database"]
-        +";UID="
-        +st.secrets["username"]
-        +";PWD="
-        +st.secrets["password"]
-       
-    )
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
 
 conn = init_connection()
 
