516a5232481fac4f18a0ffc8984f19f5f4661632
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-04-29 22:02:48 +0100

    Update financial.py

--- financial.py
+++ financial.py
@@ -1,60 +1,32 @@
 import streamlit as st
-import datetime
-import pyodbc
 import pandas as pd
-import pip
-# auth = "ActiveDirectoryPassword"
-# driver = "{ODBC Driver 17 for SQL Server}"
-# f'Driver={driver};Server={server};Database={database};Uid={username};Pwd={password};Encrypt=Yes;TrustServerCertificate=No;Authentication={auth};'
-# 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password
-
+import datetime
+from deta import Deta
 
 st.header("Pink Data Hub Financial App")
 
-pink_data = st.sidebar.selectbox("choose", ('Choose','Database','Database Connection'))
- 
-          
+
+pink_data = st.sidebar.selectbox("choose", ('Choose', 'Database', 'Database Connection'))
+
+
 if pink_data == 'Database':
-          with st.form("Submit", clear_on_submit=True):
-               st.text_input("Company's ID")
-               st.date_input("Today's Date",
-                         datetime.date(2022, 4, 1 ))
-               st.text_input("Company's Name")
-               st.text_input("Amount")
-               st.text_input("Payment Mode")
-               
-               submit = st.form_submit_button("Submit")
-               if submit:
-                   st.success("Submitted Successfully")
-            
-@st.experimental_singleton
-def pink_data():
-    return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        +st.secrets["server"]
-        +";DATABASE="
-        +st.secrets["database"]
-        +";UID="
-        +st.secrets["username"]
-        +";PWD="
-        +st.secrets["password"]
-    )
-conn = pink_data()
-
-
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+    form = st.form("forms", clear_on_submit=True)
+    id = form.text_input("Company's ID")
+    date = form.date_input("Today's Date",
+                      datetime.date(2022, 4, 1))
+    name = form.text_input("Company's Name")
+    amount = form.text_input("Amount")
+    payment = form.text_input("Payment Mode")
+    submit = form.form_submit_button("Submit")
     
     
-     
-if pink_data == 'Database Connection':
-    st.header("Database Records")
-    rows = run_query("SELECT * FROM dbo.company_details;")
-    i = 0
-    for row in rows:
-        i += 1
-        st.write(f"{i}. User : {row[0]} and Pass : {row[1]}:")
+deta = Deta.Base(st.secrets["deta_key"])
         
+db = deta.Base("Financial-records")
+
+if submit:
+    db.put({"company_id": id, "date": date, "company_name":name, "amount":amount, "payment_mode":payment})
+  
+if pink_data ==  'Database Connection':
+     db_content = db.fetch().items
+     st.write(db_content)
