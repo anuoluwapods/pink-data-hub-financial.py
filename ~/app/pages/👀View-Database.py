55f387462c5dd300667b26cf85449030276aed81
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-07-01 00:30:37 +0100

    Update 👀View-Database.py

--- ~/app/pages/👀View-Database.py
+++ ~/app/pages/👀View-Database.py
@@ -5,7 +5,7 @@ import base64
 from PIL import Image
 
 col1, col2 = st.columns(2)
-col3, col4 = st.columns(2)
+
 
 
 image = Image.open('image.png')
@@ -13,6 +13,10 @@ image = Image.open('image.png')
 
 col1.header("Pink Data Hub Financial App")
 col2.image(image)
+
+data = {"company_id": id, "date": date, "company_name":name, "amount":amount, "payment_mode":payment}
+df = df.append(data, ignore_index=True)
+df.to_csv("company_details.csv", index=False, encoding="utf-8")
 
 df = pd.read_csv("company_details.csv")
 st.markdown("Data Record: ")
