b187983f7b0f52fd35e931080c048eb36bdf2332
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-07-01 04:07:09 +0100

    Update 📥Form-Page.py

--- ~/app/📥Form-Page.py
+++ ~/app/📥Form-Page.py
@@ -16,16 +16,16 @@ col1.write("A Form Web App With Downloadable CSV File")
 col2.image(image)
 
 
-
-with st.form("forms", clear_on_submit=True):
-    id = col3.text_input("Company's ID")
-    date = col4.date_input("Today's Date",
+try:
+   with st.form("forms", clear_on_submit=True):
+        id = col3.text_input("Company's ID")
+        date = col4.date_input("Today's Date",
                       datetime.date(2022, 4, 1))
-    name = col3.text_input("Company's Name")
-    amount = col4.text_input("Amount")
-    payment = col3.text_input("Payment Mode")
-    submit = st.form_submit_button("Submit")
-    if submit:
+        name = col3.text_input("Company's Name")
+        amount = col4.text_input("Amount")
+        payment = col3.text_input("Payment Mode")
+        submit = st.form_submit_button("Submit")
+        if submit:
             st.success("Submitted Successfully")
             data = {"company_id": id, "date": date, "company_name":name, "amount":amount, "payment_mode":payment}
             df = df.append(data, ignore_index=True)
