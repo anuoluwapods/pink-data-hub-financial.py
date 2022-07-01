8975e59571dac47b35f562bc8caf29a71a92413e
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-07-01 04:02:14 +0100

    Update 👀View-Database.py

--- ~/app/pages/👀View-Database.py
+++ ~/app/pages/👀View-Database.py
@@ -14,8 +14,7 @@ image = Image.open('image.png')
 col1.header("Pink Data Hub Financial App")
 col2.image(image)
 
-data = {"company_id": id, "date": date, "company_name":name, "amount":amount, "payment_mode":payment}
-df = df.append(data, ignore_index=True)
+df.append(data, ignore_index=True)
 df.to_csv("company_details.csv", index=False, encoding="utf-8")
 
 df = pd.read_csv("company_details.csv")
