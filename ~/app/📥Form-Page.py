5c0f3f249a9713b565afa4386608213cd8dcf85f
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-08 13:18:02 +0100

    Update 📥Form-Page.py

--- ~/app/📥Form-Page.py
+++ ~/app/📥Form-Page.py
@@ -32,6 +32,8 @@ try:
         if submit:
             st.success("Submitted Successfully")
             data = {"company_id": id, "date": date, "company_name":name, "amount":amount, "payment_mode":payment}
+            df = df.append(data, ignore_index=True)
+            df.to_csv("company_details.csv", index=False, encoding="utf-8"
             df = SessionState.get(df=data)
             file_name = "financial_records.csv"
             file_path = f"./{file_name}"
@@ -44,5 +46,5 @@ try:
                                file_name=file_name,
                                key='download_df')
             df.close()
-            df = df.append(data, ignore_index=True)
-            df.to_csv("company_details.csv", index=False, encoding="utf-8")
+            )
+            
